20739792503d3196edbab11176d3d4674d9d30e2
Eitan Peles <epeles@ymail.com>
2018-06-27 13:56:25 -0300

    Update multa (if).py
    add f

--- multa (if).py
+++ multa (if).py
@@ -2,6 +2,6 @@
 vel = float(input('Qual foi a velocidade do carro (em Km)? '))
 multa = (vel - 80) * 7
 if vel > 80:
-    print('Voce foi multado em R$ {}'.format(multa))
+    print(f'Voce foi multado em R$ {multa}')
 else:
     print('Voce NAO foi multado!')
