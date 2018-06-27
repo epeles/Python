4eba0d03ffaffc1264992577ee05e7f14dbbc9db
Eitan Peles <epeles@ymail.com>
2018-06-27 14:00:30 -0300

    Update EmprestimoCasa (if).py

--- EmprestimoCasa (if).py
+++ EmprestimoCasa (if).py
@@ -3,8 +3,7 @@ salario = float(input('Qual é o seu salário? '))
 anos = int(input('Em qts anos vc ira pagar? '))
 prestacao = (casa / (anos*12))
 porcent = salario * 0.3
-#print('O valor da casa é de R$ {} que será quitado em {} meses. Valor de cada mês: R$ {:.2f}'.format(casa,anos*12,prestacao))
 if prestacao > porcent:
     print('Seu empréstimo foi negado (Excedeu 30% do seu salário)!')
 else:
-    print('Seu empréstimo foi aprovado! Serão pagos R$ {:.2f} em {} meses'.format(prestacao,anos*12))
+    print(f'Seu empréstimo foi aprovado! Serão pagos R$ {prestacao:.2f} em {anos*12} meses)
