29d2c1d2c0b1fe002eec17a640c5524bcfdb60be
Eitan Peles <epeles@ymail.com>
2018-06-27 22:55:37 -0300

    Update ProgressaoAritmetica.py

--- ProgressaoAritmetica.py
+++ ProgressaoAritmetica.py
@@ -1,6 +1,7 @@
 #Exibir a sequencia de 10 numeros de uma PA:
-elem = int(input('Digite o elemento: '))
-const = int(input('Digite a constante: '))
-dez = elem + (11) * const
-for c in range(elem,dez,const):
-    print(f'{c}', end='-> ')
+elem = int(input('Informe o elemento da PA: '))
+const = int(input('Informe a constante da PA: '))
+for c in range(1, 11):
+    pa = elem + (c-1) * const
+    print(f'{pa}', end='-> ')
+print('END')
