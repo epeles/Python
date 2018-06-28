0d6c5af9a8e5215e50ad932df7ec30ab593761eb
Eitan Peles <epeles@ymail.com>
2018-06-27 21:55:51 -0300

    Add files via upload

--- ex56.py
+++ ex56.py
@@ -1,7 +1,11 @@
+nomejovem = 0
+nomevelho = 0
 maior = 0
 menor = 1000
+maisnova = 1000
 soma = 0
 jovem = 0
+id = 0
 qtde = int(input('Qts pessoas vc deseja comparar? '))
 for c in range(1, qtde+1):
     nome = str(input(f'Digite o nome da {c}a pessoa: ')).capitalize()
@@ -12,14 +16,18 @@ for c in range(1, qtde+1):
         maior = idade
     if idade < menor:
         menor = idade
-    if maior and sexo == 'M':
-        id = maior
-        velho = nome
+    if idade > id and sexo == 'M':
+        id = idade
+        nomevelho = nome
     if idade < 20 and sexo == 'F':
         jovem += 1
+    if idade < maisnova and sexo == 'F':
         nomejovem = nome
-        maisjovem = menor
+        maisnova = idade
 media = soma / qtde
+if sexo == 'M':
+    print(f'O nome do homem mais velho é: {nomevelho}, que tem {id} anos')
+if sexo == 'F':
+    print(f'O número de mulheres abaixo de 20 anos é: {jovem}, sendo que a mais jovem se chama {nomejovem} e tem {maisnova} anos')
 print(f'A média de idade das {qtde} pessoas: {media:.2f}')
-print(f'O nome do homem mais velho é: {velho}, que tem {id} anos')
-print(f'O número de mulheres abaixo de 20 anos é: {jovem} ({nomejovem}), sendo que a mais jovem tem {menor} anos')
+print(f'A pessoa mais velha tem {maior} anos e a mais jovem tem {menor}')
