4e474f3176d7de87f260a1395c25fe5e4d56046d
Eitan Peles <epeles@ymail.com>
2018-07-22 22:21:06 -0300

    Delete Pedagio (if).py

--- Pedagio (if).py
+++
@@ -1,5 +0,0 @@
-vel = float(input('Qual foi a velocidade? '))
-if vel <= 200:
-    print('Seu pedágio foi de R$ {}'.format(vel * 0.5))
-else:
-    print('Seu pedágio foi de R$ {}'.format(vel * 0.45))
