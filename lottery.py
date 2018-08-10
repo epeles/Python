ac00148f3eae9a78d25125bcd6e34cef4b4a3030
Eitan Peles <epeles@ymail.com>
2018-08-10 12:25:40 -0300

    Add files via upload

---
+++ lottery.py
@@ -0,0 +1,23 @@
+from random import randint
+from time import sleep
+def lottery(qtty):
+    lista = []
+    games = []
+  #  qtty = int(input('How many tickets would you like to buy? '))
+    for c in range(1,qtty+1):
+        cont = 0
+        while True:
+            num = randint(1,60)
+            if num not in lista:
+                lista.append(num)
+                cont += 1
+            if cont >= 6:
+                break
+        lista.sort()
+        games.append(lista[:])
+        lista.clear()
+    for pos, number in enumerate(games):
+        print(f'Ticket {pos+1}: {number}')
+        sleep(1)
+
+lottery(5)
